81f6cb5445c7fefe503d26ff4d035a98048758bd
Soufiane <benlamalem.soufiane@gmail.com>
2017-03-23 14:38:17 +0000

    tables + remove team members workaround

--- src/app/components/reusable-components/editable-string.component.ts
+++ src/app/components/reusable-components/editable-string.component.ts
@@ -5,40 +5,27 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-editable-string',
   templateUrl: './editable-string.component.html',
-  encapsulation: ViewEncapsulation.None//,
-  //styleUrls: ['../../assets/styles/pages/editable-string.scss']
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['../../../assets/styles/components/reusable-components/editable-string.scss']
 })
-export class EditableStringComponent implements AfterViewInit, AfterViewChecked {
+export class EditableStringComponent {
 
-  @ViewChild('textarea') textarea;
+  @ViewChild('input') input;
   @Input() text:string = '';
-  @Input() name:string = 'textarea';
-  @Input() placeholder:string = 'Enter some text...';
+  @Input() name:string = 'input';
+  @Input() placeholder:string = '';
   @Output() changed = new EventEmitter<string>();
 
   constructor(private _renderer:Renderer) {
   }
 
-  ngAfterViewInit() {
-    this._adjustHeight();
-  }
-
-  ngAfterViewChecked() {
-    this._adjustHeight();
-  }
-
   onChange(text:string) {
     this.text = text;
     this.changed.emit(this.text);
-    this._adjustHeight();
+    //this._renderer.setElementStyle(this.input.nativeElement, 'width', (this.text.length + 1) + 'px');
   }
 
   onFocus() {
-    this._adjustHeight();
   }
 
-  private _adjustHeight() {
-    this._renderer.setElementStyle(this.textarea.nativeElement, 'height', '24px');
-    this._renderer.setElementStyle(this.textarea.nativeElement, 'height', this.textarea.nativeElement.scrollHeight + 4 + 'px');
-  }
 }
